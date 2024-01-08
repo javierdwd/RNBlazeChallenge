3869dc9aa5e9d2eb0e631d15506d8f1931a25d06
Javier Van Breedan <javierdwd@gmail.com>
2024-01-08 12:27:02 -0300

    [Navigation] Add @react-navigation library and patch to fix the tests

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   preset: 'react-native',
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native(-.*)?|@react-native(-community)?|@react-navigation)/)',
+  ],
+  setupFiles: ['./jest.setup.js'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
 };
