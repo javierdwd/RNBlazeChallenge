9cba8b078a246484ae0fa071e0aa4aff0c8ee93f
Javier Van Breedan <javierdwd@gmail.com>
2024-01-10 10:51:43 -0300

    [UIKit] Remove unused dependencies

--- src/UIKit/CTAPanel/CTAPanel.test.tsx
+++ src/UIKit/CTAPanel/CTAPanel.test.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 import {CTAPanel} from './CTAPanel';
 
 // Note: import explicitly to use the types shipped with jest.
-import {it, jest, expect} from '@jest/globals';
+import {it, jest} from '@jest/globals';
 
 // Note: test renderer must be required after react-native.
-import {render, fireEvent, screen} from '@testing-library/react-native';
+import {render} from '@testing-library/react-native';
 
 it('renders CTAPanel correctly', async () => {
   const mockFn = jest.fn();
