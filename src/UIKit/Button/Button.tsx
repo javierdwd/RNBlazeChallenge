04373b89b97300e8b1da9086f6f5079ac607f0ce
Javier Van Breedan <javierdwd@gmail.com>
2024-01-10 10:02:19 -0300

    [UIKit] Button: Add support for variants (colors)

--- src/UIKit/Button/Button.tsx
+++ src/UIKit/Button/Button.tsx
@@ -1,15 +1,75 @@
 import React from 'react';
-import {TouchableOpacity, Text} from 'react-native';
+import {StyleSheet, TouchableOpacity, Text} from 'react-native';
 import type {TouchableOpacityProps} from 'react-native';
+
+import {Colors} from '../Colors';
 
 type Props = {
   text: string;
+  variant?: 'neutral' | 'info' | 'success' | 'warning' | 'danger';
 } & TouchableOpacityProps;
 
-export function Button(props: Props) {
+const localStyles = StyleSheet.create({
+  container: {
+    alignSelf: 'flex-start',
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    borderWidth: 3,
+    borderRadius: 4,
+    width: 'auto',
+    flexGrow: 0,
+  },
+  text: {
+    fontSize: 15,
+  },
+
+  // Variants:
+  neutral: {
+    backgroundColor: Colors.grey,
+    borderColor: Colors.grey,
+  },
+  info: {
+    backgroundColor: Colors.blue,
+    borderColor: Colors.blue,
+  },
+  success: {
+    backgroundColor: Colors.green,
+    borderColor: Colors.green,
+  },
+  warning: {
+    backgroundColor: Colors.yellow,
+    borderColor: Colors.yellow,
+  },
+  danger: {
+    backgroundColor: Colors.red,
+    borderColor: Colors.red,
+  },
+
+  neutralText: {
+    color: Colors.black,
+  },
+  infoText: {
+    color: Colors.white,
+  },
+  successText: {
+    color: Colors.white,
+  },
+  warningText: {
+    color: Colors.black,
+  },
+  dangerText: {
+    color: Colors.white,
+  },
+});
+
+export function Button({variant = 'neutral', ...props}: Props) {
   return (
-    <TouchableOpacity {...props}>
-      <Text>{props.text}</Text>
+    <TouchableOpacity
+      {...props}
+      style={[localStyles.container, localStyles[variant], props.style]}>
+      <Text style={[localStyles.text, localStyles[`${variant}Text`]]}>
+        {props.text}
+      </Text>
     </TouchableOpacity>
   );
 }
---
+++ src/UIKit/Colors.ts
@@ -0,0 +1,10 @@
+export const Colors = {
+  brand: '#34d399',
+  white: '#FFFFFF',
+  black: '#333333',
+  grey: '#D4D4D4',
+  green: '#22C55e',
+  blue: '#06B6D4',
+  yellow: '#FCD34d',
+  red: '#DC2626',
+};
